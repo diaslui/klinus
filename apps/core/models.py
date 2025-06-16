125b32b942d38f01df88efefdd8c63577ca20cff
Victor Manuel LR <vleal3423@gmail.com>
2025-06-16 13:31:05 -0300

    feat: add Address, Instituition, Campus

--- apps/core/models.py
+++ apps/core/models.py
@@ -6,4 +6,20 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True 
 
-    
+
+class Address(BaseModel):
+    street = models.CharField(null=False, blank=False, max_length=120)
+    number = models.CharField(null=True, blank=True, max_length=5)
+    neighborhood = models.CharField(null=True, blank=True, max_length=80)
+    city = models.CharField(null=True, blank=True, max_length=80)
+    state = models.CharField(null=True, blank=True, max_length=30)
+
+
+class Institution(BaseModel):
+    full_name = models.CharField(null=False, blank=False, max_length=120)
+    acronym = models.CharField(null=False, blank=False, max_length=25)
+
+
+class Campus(BaseModel):
+    address = models.OneToOneField(Address)
+    name = models.CharField(null=False, blank=False, max_length=80)
