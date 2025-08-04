dfa5fb6664eaeffba2ddaa06cd33f581cd1e6a23
Pedro Luis Dias <luisp.diias@gmail.com>
2025-08-04 19:41:15 -0300

    feat: add server mode functionality to firmware

--- firmware/main.ino
+++ firmware/main.ino
@@ -1,4 +1,27 @@
+/*
+ * Firmware - Klinus
+ * https://github.com/diaslui/klinus/
+ * Copyright 2025 Pedro Luis Dias
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  https://github.com/diaslui/klinus/blob/main/LICENSE
+ */
+
+
 #include <config.hpp>
+
+int SERVER_MODE = true;
+
 
 void setup(){
 
@@ -6,6 +29,13 @@ void setup(){
 }
 
 void loop(){
+
+    if (SERVER_MODE){
+        Serial.printf('Server Mode');
+    }else{
+        Serial.printf('Local Mode');
+
+    }
 
 
     
